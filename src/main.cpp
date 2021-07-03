3c70d013b42bbb991bdb4e8cc9f0942dfef13ed4
unknown <hygor.officer@gmail.com>
2021-07-03 01:15:41 -0300

    Implementação da flag com contator

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,12 @@
 #define OLED_SCL 15
 #define OLED_RESET 16
 
+int temp = 0, counter = 0,
+    temp_flag = 2000;
+
+
+
+
 SSD1306 OLED(OLED_ADDRES,OLED_SDA,OLED_SCL);
 
 void display_config()
@@ -26,10 +32,27 @@ void display_config()
 
 void Logica()
 {
+
+  if (millis() - temp >= temp_flag)
+  {
+    digitalWrite(flag,!digitalRead(flag));
+    temp = millis();
+    
+  }
+  if (digitalRead(flag) == HIGH)
+  {
+    OLED.clear();
+    OLED.drawString(0,0,"Flag Ligada");
+    OLED.display();
+    delay(temp_flag);
+    counter++;
+  }
+  
   OLED.clear();
-  OLED.drawString(0,0,"Hello Word!");
+  OLED.drawString(0,0,"Flag Desligada");
+  OLED.drawString(0,20,"Qtde Acion. " + String(counter) );
   OLED.display();
-  delay(1000);
+  
   
 
 }
