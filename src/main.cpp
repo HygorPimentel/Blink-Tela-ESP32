9e8241e299a21d0f47b4634a3dc7a0ab1cdac8ec
unknown <hygor.officer@gmail.com>
2021-07-03 00:59:46 -0300

    Implementação do display OLED

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,48 @@
 #include <Arduino.h>
+#include <SSD1306.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define flag 25
+#define OLED_ADDRES 0x3c
+#define OLED_SDA 4
+#define OLED_SCL 15
+#define OLED_RESET 16
+
+SSD1306 OLED(OLED_ADDRES,OLED_SDA,OLED_SCL);
+
+void display_config()
+{
+  pinMode(OLED_RESET,OUTPUT);
+  digitalWrite(OLED_RESET,LOW);
+  delay(10);
+  digitalWrite(OLED_RESET,HIGH);
+  delay(10);
+
+  OLED.init();
+
+  OLED.flipScreenVertically();
+  OLED.setFont(ArialMT_Plain_16);
+  OLED.setTextAlignment(TEXT_ALIGN_LEFT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void Logica()
+{
+  OLED.clear();
+  OLED.drawString(0,0,"Hello Word!");
+  OLED.display();
+  delay(1000);
+  
+
+}
+
+
+void setup() 
+{
+  display_config();
+  pinMode(flag,OUTPUT);
+  digitalWrite(flag,LOW);
+}
+
+void loop()
+{
+  Logica();
 }
